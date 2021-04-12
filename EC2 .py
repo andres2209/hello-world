f4df8f7b7fc79ead52207f456e5863086654a007
andres2209 <82000929+andres2209@users.noreply.github.com>
2021-04-12 15:41:52 -0500

    Add files via upload

---
+++ EC2 .py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np 
+import altair as alt
+
+
+# In[1]:
+
+
+nombre = "Andrés"
+print("Hola, ya terminé el EC2. Saludos. " + nombre)
+
+
+# In[ ]:
+
+
+
+
